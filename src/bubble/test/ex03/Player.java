fe14147342f5d235d79f0e1942c8cd89ff5693a7
mouse <hyeji111544@gmail.com>
2024-10-22 01:14:19 +0900

    플레이어 좌우 이동

---
+++ src/bubble/test/ex03/Player.java
@@ -0,0 +1,72 @@
+package bubble.test.ex03;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+// class Player -> 메모리에 띄울 수 있는 애들. 게임에 존재할 수 있다. (추상메서드를 가질 수 없다. 따라서 무조건 구현해 줄 의무를 가짐) 
+public class Player extends JLabel implements Moveable {
+
+	
+	// 위치 상태
+	private int x;
+	private int y;
+	
+	// 움직임 상태
+	private boolean left;
+	private boolean right;
+	private boolean up;
+	private boolean down;
+	
+	private ImageIcon playerR, playerL;
+
+	public Player() {
+		initObject();
+		initSetting();
+	}
+
+	private void initObject() {
+		playerR = new ImageIcon("image/playerR.png");
+		playerL = new ImageIcon("image/playerL.png");
+	}
+
+	private void initSetting() {
+		x = 55;
+		y = 535;
+		
+		left = false;
+		right = false;
+		up = false;
+		down = false;
+
+		this.setIcon(playerR);
+		setSize(50, 50);
+		setLocation(x, y);
+	}
+
+	@Override
+	public void left() {
+		setIcon(playerL);
+		x = x-10;
+		setLocation(x, y);
+	}
+
+	@Override
+	public void right() {
+		setIcon(playerR);
+		x = x+10;
+		setLocation(x, y);
+	}
+
+	@Override
+	public void up() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void down() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
