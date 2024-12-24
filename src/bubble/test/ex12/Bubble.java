5cdf0fe43fd3a134ec05678c26d994c4fa7aa0ee
mouse <hyeji111544@gmail.com>
2024-12-24 15:50:15 +0900

    ex12. 방울 벽 충돌감지하면 멈추기

---
+++ src/bubble/test/ex12/Bubble.java
@@ -0,0 +1,130 @@
+package bubble.test.ex12;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Bubble extends JLabel implements Moveable{
+
+	//의존성 콤포지션
+	private Player player;
+	private BackGroundBubbleService backGroundBubbleService;
+	
+	// 위치 상태
+	private int x;
+	private int y;
+	
+	// 움직임 상태
+	private boolean left;
+	private boolean right;
+	private boolean up;
+	
+	// 적군을 맞춘 상태
+	private int state; // 0(물방울), 1(적을 가둔 물방울)
+	
+	private ImageIcon bubble; // 물방울
+	private ImageIcon bubbled; // 적을 가둔 물방울
+	private ImageIcon bomb; // 물방울이 터진거
+	
+	public Bubble(Player player) {
+		this.player = player;
+		initObject();
+		initSetting();
+		initThread();
+	}
+	
+	private void initObject() {
+		bubble = new ImageIcon("image/bubble.png");
+		bubbled = new ImageIcon("image/bubbled.png");
+		bomb = new ImageIcon("image/bomb.png");
+		
+		backGroundBubbleService = new BackGroundBubbleService(this);
+	}
+	
+	private void initSetting() {
+		left = false;
+		right = false;
+		up = false;
+		
+		x = player.getX();
+		y = player.getY();
+		
+		setIcon(bubble);
+		setSize(50, 50);
+		state = 0;
+	}
+	
+	private void initThread() {
+		new Thread(()-> {
+			 System.out.println("스레드 실행 시작");
+			if(player.getPlayerDirection()==PlayerDirection.LEFT) {
+				left();
+			}else {
+				right();
+			}
+			System.out.println("스레드 실행 완료");
+		}) .start();
+	}
+
+	@Override
+	public void left() {
+		left = true;
+		for(int i=0; i<400; i++) {
+			x--;
+			setLocation(x, y);
+			
+			if(backGroundBubbleService.leftWall()) {
+				break;
+			}
+			
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		up();
+	}
+
+	@Override
+	public void right() {
+		right = true;
+		for(int i=0; i<400; i++) {
+			x++;
+			setLocation(x, y);
+			
+			if(backGroundBubbleService.rightWall()) {
+				break;
+			}
+			
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		up();
+	}
+
+	@Override
+	public void up() {
+		while(true) {
+			y--;
+			setLocation(x, y);
+			
+			if(backGroundBubbleService.topWall()) {
+				break;
+			}
+			
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ src/bubble/test/ex12/Moveable.java
@@ -0,0 +1,8 @@
+package bubble.test.ex12;
+
+public interface Moveable {
+	public abstract void left();
+	public abstract void right();
+	public abstract void up();
+	default public void down() {};
+}
