b0706e79848a7e87034affb6ecdf056731ded412
mouse <hyeji111544@gmail.com>
2024-12-24 17:26:20 +0900

    ex13. 코드 리팩토링

---
+++ src/bubble/test/ex13/Bubble.java
@@ -0,0 +1,137 @@
+package bubble.test.ex13;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Bubble extends JLabel implements Moveable{
+
+	//의존성 콤포지션
+	private BubbleFrame mContext;
+	private Player player;
+	private BackGroundBubbleService backGroundBubbleService;
+	
+	// 위치 상태
+	private int x;
+	private int y;
+	
+	// 움직임 상태
+	private boolean left;
+	private boolean right;
+	private boolean up;
+	
+	// 적군을 맞춘 상태
+	private int state; // 0(물방울), 1(적을 가둔 물방울)
+	
+	private ImageIcon bubble; // 물방울
+	private ImageIcon bubbled; // 적을 가둔 물방울
+	private ImageIcon bomb; // 물방울이 터진거
+	
+	public Bubble(BubbleFrame mContext) {
+		this.mContext = mContext;
+		this.player = mContext.getPlayer();
+		initObject();
+		initSetting();
+	}
+	
+	private void initObject() {
+		bubble = new ImageIcon("image/bubble.png");
+		bubbled = new ImageIcon("image/bubbled.png");
+		bomb = new ImageIcon("image/bomb.png");
+		
+		backGroundBubbleService = new BackGroundBubbleService(this);
+	}
+	
+	private void initSetting() {
+		left = false;
+		right = false;
+		up = false;
+		
+		x = player.getX();
+		y = player.getY();
+		
+		setIcon(bubble);
+		setSize(50, 50);
+		state = 0;
+	}
+	
+
+	@Override
+	public void left() {
+		left = true;
+		for(int i=0; i<400; i++) {
+			x--;
+			setLocation(x, y);
+			
+			if(backGroundBubbleService.leftWall()) {
+				left = false;
+				break;
+			}
+			
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		up();
+	}
+
+	@Override
+	public void right() {
+		right = true;
+		for(int i=0; i<400; i++) {
+			x++;
+			setLocation(x, y);
+			
+			if(backGroundBubbleService.rightWall()) {
+				right = false;
+				break;
+			}
+			
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		up();
+	}
+
+	@Override
+	public void up() {
+		while(true) {
+			y--;
+			setLocation(x, y);
+			
+			if(backGroundBubbleService.topWall()) {
+				break;
+			}
+			
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		clearBubble(); // 천장에 버블이 도착하고 나서 3초 후에 메모리에서 소멸
+	}
+	
+	private void clearBubble() {
+		try {
+			Thread.sleep(3000);
+			setIcon(bomb);
+			Thread.sleep(500);
+			mContext.remove(this); // BubbleFrame 의 메모리에서 사라짐
+			mContext.repaint();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
